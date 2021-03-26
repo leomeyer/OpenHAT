9f7cfd95f46b8b9a13660265b1aa6c286bef3177
Leo Meyer <leo@leomeyer.de>
2021-03-26 12:01:47 +0100

    Add missing file

---
+++ openhatd/src/TypeGUIDs.h
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   TypeGUIDs.h
+ * Author: leo
+ *
+ * Created on March 26, 2021, 7:02 AM
+ */
+
+#ifndef TYPEGUIDS_H
+#define TYPEGUIDS_H
+
+#define OPDI_CUSTOM_PORT_TYPEGUID        "ae56d9a2-48e8-4275-ab4e-a5961887bf41"
+
+
+#endif /* TYPEGUIDS_H */
+
